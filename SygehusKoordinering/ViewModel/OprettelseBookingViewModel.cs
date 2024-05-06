96b7de3fd41343bc7a1c9416ba6c980ca7c53776
Yaruohri <bendixforsberg@hotmail.com>
2024-05-06 18:53:29 +0200

    Update OprettelseBookingViewModel.cs

--- SygehusKoordinering/ViewModel/OprettelseBookingViewModel.cs
+++ SygehusKoordinering/ViewModel/OprettelseBookingViewModel.cs
@@ -18,6 +18,8 @@ namespace SygehusKoordinering.ViewModel
         public OprettelseBookingViewModel()
         {
             LoadAfdeling();
+            LoadProeve();
+            LoadSaerligeForhold();
         }
 
         [ObservableProperty]
